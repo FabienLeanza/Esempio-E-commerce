3bf5a09e6a3dcc8ba065f2ba074adc8ec23686e7
Fabien <leanzafabien@gmail.com>
2025-06-11 12:00:00 +0200

    esempio e commerce

---
+++ app.js
@@ -0,0 +1,53 @@
+// Array di oggetti (libri)
+const libri = [
+  {
+    id: 1,
+    titolo: "Ventimila Leghe Sotto i Mari",
+    autore: "Jules Verne",
+    anno: 1870,
+    prezzo: 169.9,
+    immagine: "assets/verne.webp",
+    inStock: true,
+  },
+  {
+    id: 2,
+    titolo: "Frankenstein",
+    autore: "Mary Shelley",
+    prezzo: 114.9,
+    immagine: "assets/frankenstein.webp",
+    inStock: true,
+  },
+  {
+    id: 3,
+    titolo: "Alice nel paese delle meraviglie",
+    autore: "Lewis Carroll",
+    prezzo: 99.9,
+
+    immagine: "assets/alice.webp",
+    inStock: false,
+  },
+  {
+    id: 4,
+    titolo: "Dracula",
+    autore: "Bram Stocker",
+    prezzo: 134.9,
+    immagine: "assets/dracula.webp",
+    inStock: true,
+  },
+  {
+    id: 5,
+    titolo: "La Divina Commedia",
+    autore: "Dante Alighieri",
+    prezzo: 249.9,
+    immagine: "assets/commedia.webp",
+    inStock: true,
+  },
+  {
+    id: 6,
+    titolo: "Il Signore degli Anelli",
+    autore: "J.R.R. Tolkien",
+    prezzo: 329.9,
+    immagine: "assets/rings.webp",
+    inStock: true,
+  },
+];
